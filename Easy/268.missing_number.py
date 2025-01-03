3c69e99e6a6d39049602284e8ae391fd8fa05177
Chaitrali Deshpande <chaitralideshpande@Chaitralis-MacBook-Air.local>
2025-01-03 11:41:51 -0800

    leetcode

---
+++ Easy/268.missing_number.py
@@ -0,0 +1,32 @@
+'''
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+Example 1:
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+'''
+
+class Solution:
+    def missingNumber(self, nums):
+        num_set = set(nums)
+        n = len(nums) + 1
+        for number in range(n):
+            if number not in num_set:
+                return number
+            
+class Solution:
+    def missingNumber(self, nums):
+        nums.sort()
+
+        # Ensure that n is at the last index
+        if nums[-1] != len(nums):
+            return len(nums)
+        # Ensure that 0 is at the first index
+        elif nums[0] != 0:
+            return 0
+
+        # If we get here, then the missing number is on the range (0, n)
+        for i in range(1, len(nums)):
+            expected_num = nums[i-1] + 1
+            if nums[i] != expected_num:
+                return expected_num
